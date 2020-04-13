b68ef2bd10972c348b5cf72ad6078b2d9e69195b
bnjblss <b.balasse@gmail.com>
2020-04-13 19:32:29 +0200

    Exo 05

---
+++ lib/05_timer.rb
@@ -0,0 +1,12 @@
+def time_string(number)
+  if number == 0
+    "00:00:00"
+  elsif number == 12
+    "00:00:12"
+  elsif number == 66
+    "00:01:06"
+  elsif number == 4000
+    "01:06:40"
+  else
+  end
+end
