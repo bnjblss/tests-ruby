5452cd47d11951a2b8e1e56e678be84d6cab9ddb
bnjblss <b.balasse@gmail.com>
2020-04-13 18:32:30 +0200

    Modifs exo 03

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,5 +1,24 @@
-def who_is_bigger(max)
-
+def who_is_bigger(a, b, c)
+  if a == nil || b== nil || c== nil
+    return "nil detected"
+  elsif a > b && a > c
+    return "a is bigger"
+  elsif b > a && b>c
+    return "b is bigger"
+  elsif c>a && c>b
+    return "c is bigger"
+  end
 end
 
-who_is_bigger(2, 4)
+def reverse_upcase_noLTA(string)
+  string.upcase.reverse.delete("LTA")
+end
+
+def array_42(array)
+  array.include?(42)
+end
+
+def magic_array(arrays)
+  arrays.map{|number| number*2}
+
+end
